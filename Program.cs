3885d0dd2896868f82840eedf4a7a4125440b6a8
Andrea Riley <andreariley@me.com>
2024-05-02 14:13:54 -0400

    azure exercise

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace az204_auth
+{
+    class Program
+    {
+        private const string _clientId = "698d0880-1fc0-4849-8dcb-ca5959fdd07b";
+        private const string _tenantId = "4baf5006-a8e0-48d3-8fa3-cbfdd7837eed";
+        public static async Task Main(string[] args)
+        {
+            var app = PublicClientApplicationBuilder
+                .Create(_clientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+                .WithRedirectUri("http://localhost")
+                .Build();
+            string[] scopes = { "user.read" };
+            AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+            Console.WriteLine($"Token:\t{result.AccessToken}");
+
+        }
+
+    }
+}
+
+
